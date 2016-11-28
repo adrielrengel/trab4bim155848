1267f8d30a93aa7b9a06c882d2a707aabf0a0e3a
adrielrengel <adrielrengel@gmail.com>
2016-11-28 20:23:35 -0200

    commit 9 - Editando registros
    nesse comiti criamos a parte responsavel pela ediçao dos registros, 
    os metodos para salvar um registro editado e buscar o registro que sera
    editado
    colocamos o icone na penultima coluna da tabela

--- AppJFS/src/main/java/br/com/adriel/pessoa/controller/ConsultarPessoaController.java
+++ AppJFS/src/main/java/br/com/adriel/pessoa/controller/ConsultarPessoaController.java
@@ -2,56 +2,77 @@ package br.com.adriel.pessoa.controller;
 
 import java.io.Serializable;
 import java.util.List;
- 
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.inject.Produces;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
- 
+
 import br.com.adriel.model.PessoaModel;
 import br.com.adriel.repository.PessoaRepository;
 
 /*AUTOR: Adriel
  * esse vai ser o controller das pessoas*/
- 
-@Named(value="consultarPessoaController")
+
+@Named(value = "consultarPessoaController")
 @ViewScoped
 public class ConsultarPessoaController implements Serializable {
- 
+
 	private static final long serialVersionUID = 1L;
- 
-	@Inject transient
-	private PessoaModel pessoaModel;
- 
-	@Produces 
+
+	@Inject
+	transient private PessoaModel pessoaModel;
+
+	@Produces
 	private List<PessoaModel> pessoas;
- 
-	@Inject transient
-	private PessoaRepository pessoaRepository;
- 
+
+	@Inject
+	transient private PessoaRepository pessoaRepository;
+
 	public List<PessoaModel> getPessoas() {
 		return pessoas;
 	}
+
 	public void setPessoas(List<PessoaModel> pessoas) {
 		this.pessoas = pessoas;
-	}		
+	}
+
 	public PessoaModel getPessoaModel() {
 		return pessoaModel;
 	}
+
 	public void setPessoaModel(PessoaModel pessoaModel) {
 		this.pessoaModel = pessoaModel;
 	}
- 
+
 	/***
 	 * Carrega todas as pessoas ja criadas
 	 */
 	@PostConstruct
-	public void init(){
- 
-		//RETORNAR AS PESSOAS CADASTRADAS
+	public void init() {
+
+		// RETORNAR AS PESSOAS CADASTRADAS
 		this.pessoas = pessoaRepository.GetPessoas();
 	}
- 
- 
+
+	/* Carrega informaçoes da pessoa que vamos editar */
+	public void Editar(PessoaModel pessoaModel) {
+
+		/* PEGA APENAS A PRIMEIRA LETRA DO SEXO PARA SETAR NO CAMPO(M OU F) */
+		pessoaModel.setSexo(pessoaModel.getSexo().substring(0, 1));
+
+		this.pessoaModel = pessoaModel;
+
+	}
+
+	/* Atualiza o registro da pessoa que editamos */
+	public void AlterarRegistro() {
+
+		this.pessoaRepository.AlterarRegistro(this.pessoaModel);
+
+		/* RECARREGA OS REGISTROS */
+		this.init();
+	}
+
 }
--- AppJFS/src/main/java/br/com/adriel/repository/PessoaRepository.java
+++ AppJFS/src/main/java/br/com/adriel/repository/PessoaRepository.java
@@ -14,81 +14,105 @@ import br.com.adriel.model.UsuarioModel;
 import br.com.adriel.repository.entity.PessoaEntity;
 import br.com.adriel.repository.entity.UsuarioEntity;
 import br.com.adriel.uteis.Uteis;
+
 /*Autor: adriel
  * esse e o modelo de pessoas, terao o metodo que vai salvar a pessoa no banco */
 public class PessoaRepository {
 
 	@Inject
 	PessoaEntity pessoaEntity;
-	
+
 	EntityManager entityManager;
-	
+
 	/***
-	 * Esse será o metodo usado para salvar uma nova pessoa
-	 */
-	public void SalvarNovoRegistro(PessoaModel pessoaModel){
-		entityManager 	= Uteis.JpaEntityManager();
-		pessoaEntity = 	new PessoaEntity();
+	 *  * Esse será o metodo usado para salvar uma nova pessoa  
+	 */
+	public void SalvarNovoRegistro(PessoaModel pessoaModel) {
+		entityManager = Uteis.JpaEntityManager();
+		pessoaEntity = new PessoaEntity();
 		pessoaEntity.setDataCadastro(LocalDateTime.now());
 		pessoaEntity.setEmail(pessoaModel.getEmail());
 		pessoaEntity.setEndereco(pessoaModel.getEndereco());
 		pessoaEntity.setNome(pessoaModel.getNome());
 		pessoaEntity.setOrigemCadastro(pessoaModel.getOrigemCadastro());
 		pessoaEntity.setSexo(pessoaModel.getSexo());
-		
-	
-		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo());		
+
+		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class,
+				pessoaModel.getUsuarioModel().getCodigo());
 		pessoaEntity.setUsuarioEntity(usuarioEntity);
-		
+
 		entityManager.persist(pessoaEntity);
 	}
-	
-	/*Esse metodo vai fazer a consulta das pessoas no banco*/
 
-	public List<PessoaModel> GetPessoas(){
-		 
+	/* Esse metodo vai fazer a consulta das pessoas no banco */
+
+	public List<PessoaModel> GetPessoas() {
+
 		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
- 
-		entityManager =  Uteis.JpaEntityManager();
- 
+
+		entityManager = Uteis.JpaEntityManager();
+
 		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
- 
+
 		@SuppressWarnings("unchecked")
-		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>)query.getResultList();
- 
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>) query.getResultList();
+
 		PessoaModel pessoaModel = null;
- 
+
 		for (PessoaEntity pessoaEntity : pessoasEntity) {
- 
+
 			pessoaModel = new PessoaModel();
 			pessoaModel.setCodigo(pessoaEntity.getCodigo());
 			pessoaModel.setDataCadastro(pessoaEntity.getDataCadastro());
 			pessoaModel.setEmail(pessoaEntity.getEmail());
 			pessoaModel.setEndereco(pessoaEntity.getEndereco());
 			pessoaModel.setNome(pessoaEntity.getNome());
- 
-			if(pessoaEntity.getOrigemCadastro().equals("X"))
+
+			if (pessoaEntity.getOrigemCadastro().equals("X"))
 				pessoaModel.setOrigemCadastro("XML");
 			else
 				pessoaModel.setOrigemCadastro("INPUT");
- 
- 
-			if(pessoaEntity.getSexo().equals("M"))
+
+			if (pessoaEntity.getSexo().equals("M"))
 				pessoaModel.setSexo("Masculino");
 			else
 				pessoaModel.setSexo("Feminino");
- 
-			UsuarioEntity usuarioEntity =  pessoaEntity.getUsuarioEntity();			
- 
+
+			UsuarioEntity usuarioEntity = pessoaEntity.getUsuarioEntity();
+
 			UsuarioModel usuarioModel = new UsuarioModel();
 			usuarioModel.setUsuario(usuarioEntity.getUsuario());
- 
+
 			pessoaModel.setUsuarioModel(usuarioModel);
- 
+
 			pessoasModel.add(pessoaModel);
 		}
- 
+
 		return pessoasModel;
- 
+
 	}
+	/* Esse metodo vai fazer a consulta das pessoas */
+
+	private PessoaEntity GetPessoa(int codigo) {
+
+		entityManager = Uteis.JpaEntityManager();
+
+		return entityManager.find(PessoaEntity.class, codigo);
+	}
+
+	/* Esse metodo vai fazer a alteraçao dos registros */
+	public void AlterarRegistro(PessoaModel pessoaModel) {
+
+		entityManager = Uteis.JpaEntityManager();
+
+		PessoaEntity pessoaEntity = this.GetPessoa(pessoaModel.getCodigo());
+
+		pessoaEntity.setEmail(pessoaModel.getEmail());
+		pessoaEntity.setEndereco(pessoaModel.getEndereco());
+		pessoaEntity.setNome(pessoaModel.getNome());
+		pessoaEntity.setSexo(pessoaModel.getSexo());
+
+		entityManager.merge(pessoaEntity);
+	}
+
 }
